276256b1251b46d6ab55d58eb4709d46fecccc9e
Leo Yockey <leoyockey@gmail.com>
2020-06-05 16:21:29 -0700

    prepare for AWS

--- index.js
+++ index.js
@@ -1,98 +1,101 @@
-const AWS = require('aws-sdk');
 const request = require('request-promise-native');
+
+const AWS = require('aws-sdk');
 AWS.config.update({region: 'us-west-2'});
+const sns = new AWS.SNS({apiVersion: '2010-03-31'});
+const s3 = new AWS.S3();
+const s3Bucket = process.env.S3_BUCKET;
+const s3Key = 'highs.json';
+const s3Type = 'application/json';
+const symbols = [
+    'CVS',
+    'DRVD',
+    'EPD',
+    'ET',
+    'GKOS',
+    'GMAB',
+    'IEP',
+    'LX',
+    'MOMO',
+    'MRVL',
+    'MYOK',
+    'NEO',
+    'NVCR',
+    'PFPT',
+    'PYPL',
+    'SPG',
+    'VRTX',
+    'ZTO',
+    'GDX',
+    'SMFG',
+    'VEON',
+    'DIS'
+];
 
+const getStockPriceQuotes = async () => {
+    const url = `https://api.tradeking.com/v1/market/ext/quotes.json?fids=hi,lo,name,symbol&symbols=${symbols.join(',')}`;
+    const oauth = {
+        consumer_key: process.env.CONSUMER_KEY,
+        consumer_secret: process.env.CONSUMER_SECRET,
+        token: process.env.ACCESS_TOKEN_KEY,
+        token_secret: process.env.ACCESS_TOKEN_SECRET
+    }
+    const requestOptions = { oauth, url, json: true };
 
-//TODO: replace with s3 data
-const currentHighPrices = {
-    "CVS": "0",
-    "DRVD": "0",
-    "EPD": "0",
-    "ET": "0",
-    "GKOS": "0",
-    "GMAB": "0",
-    "IEP": "0",
-    "LX": "0",
-    "MOMO": "0",
-    "MRVL": "0",
-    "MYOK": "0",
-    "NEO": "0",
-    "NVCR": "0",
-    "PFPT": "0",
-    "PYPL": "0",
-    "SPG": "0",
-    "VRTX": "0",
-    "ZTO": "0",
-    "GDX": "0",
-    "SMFG": "0",
-    "VEON": "0",
-    "DIS": "0"
-}
+    return request(requestOptions).then(processQuotes);
+};
 
-const symbols = ['CVS',
-'DRVD',
-'EPD',
-'ET',
-'GKOS',
-'GMAB',
-'IEP',
-'LX',
-'MOMO',
-'MRVL',
-'MYOK',
-'NEO',
-'NVCR',
-'PFPT',
-'PYPL',
-'SPG',
-'VRTX',
-'ZTO',
-'GDX',
-'SMFG',
-'VEON',
-'DIS'];
-const url = `https://api.tradeking.com/v1/market/ext/quotes.json?fids=hi,lo,name,symbol&symbols=${symbols.join(',')}`;
-const oauth = {
-    consumer_key: process.env.CONSUMER_KEY,
-    consumer_secret: process.env.CONSUMER_SECRET,
-    token: process.env.ACCESS_TOKEN_KEY,
-    token_secret: process.env.ACCESS_TOKEN_SECRET
-}
-
-const requestOptions = { oauth, url, json: true };
-
-const getQuotes = () => request(requestOptions).then(processQuotes);
-
-const processQuotes = res => {
+const processQuotes = async res => {
+    currentHighPrices = await getPrices().then(r => JSON.parse(r.Body.toString()));
     const quotes = res.response.quotes.quote;
+
     const newHighs = quotes.reduce((highs, q) => {
+        console.log('q', q);
         const {hi, lo, symbol} = q;
         if (hi > highs[symbol]) {
+            console.log(`in hi for ${symbol}`);
             highs[symbol] = hi;
         } else {
             analyzeLow(symbol, lo);
         }
         return highs;
-    }, currentHighPrices)
-    sendNewHighsToS3(newHighs);
+    }, currentHighPrices);
+
+    await sendNewHighsToS3(newHighs);
 }
 
-const analyzeLow = (symbol, low) => {
+const getPrices = async () => {
+    const params = {
+        Bucket: s3Bucket,
+        Key: s3Key
+    };
+    return await s3.getObject(params).promise();
+}
+
+const sendNewHighsToS3 = async highs => {
+    if (!Object.keys(highs).length) return;
+    const destparams = {
+        Bucket: s3Bucket,
+        Key: s3Key,
+        Body: JSON.stringify(highs),
+        ContentType: s3Type
+    };
+    await s3.putObject(destparams).promise();
+}
+
+const analyzeLow = async (symbol, low) => {
     const high = currentHighPrices[symbol];
     const loss = ((low-high)/high)*100;
-
+    console.log('loss for ', symbol, ': ', loss);
     if (loss <= -25) sendAlert(symbol);
 }
 
-const sendAlert = symbol => {
+const sendAlert = async symbol => {
     const params = {
         Message: `SELL ALERT! Sell ${symbol}`,
-        TopicArn: 'arn:aws:sns:us-west-2:120597452984:LeoTexts'
+        TopicArn: process.env.TOPIC_ARN
     };
-  
-    // Create promise and SNS service object
-    const snsPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
-    snsPromise.catch(e => console.error(`Error sending message for ${symbol}`, e.message));
+    await sns.publish(params).promise();
 }
 
-getQuotes()
+exports.handler = async () => getStockPriceQuotes();
