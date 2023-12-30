d3af739b2027356101c3c375e43debbf2af7ed2c
Leo Yockey <leoyockey@gmail.com>
2023-12-30 12:43:23 -0500

    alphabetize functions

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const request = require("request-promise-native");
 const fs = require("fs");
 const highs = require("./highs.json");
 
-const isStopLoss = (symbol, low) => {
-  const high = highs[symbol];
-  const loss = ((low - high) / high) * 100;
-  return loss <= -25 && loss > -100;
+const generateHighsAndLosses = (acc, q) => {
+  const { h: hi, l: lo, symbol } = q;
+  if (!acc.highs[symbol] || hi > acc.highs[symbol]) {
+    console.log(`in high for ${symbol}: ${hi}`);
+    acc.highs[symbol] = hi;
+  } else if (isStopLoss(symbol, lo)) {
+    acc.stopLosses.push(symbol);
+  }
+  return acc;
 };
 
 const getStockPriceQuotes = async () => {
@@ -21,24 +26,13 @@ const getStockPriceQuotes = async () => {
   return Promise.all(allQuotes).then(processQuotes).catch(logError);
 };
 
+const isStopLoss = (symbol, low) => {
+  const high = highs[symbol];
+  const loss = ((low - high) / high) * 100;
+  return loss <= -25 && loss > -100;
+};
+
 const logError = (e) => console.error(e.message);
-
-const updateHighs = (newHighs) => {
-  fs.writeFile("./highs.json", JSON.stringify(newHighs), "utf8", (err) => {
-    if (err) return console.error(err);
-  });
-};
-
-const generateHighsAndLosses = (acc, q) => {
-  const { h: hi, l: lo, symbol } = q;
-  if (!acc.highs[symbol] || hi > acc.highs[symbol]) {
-    console.log(`in high for ${symbol}: ${hi}`);
-    acc.highs[symbol] = hi;
-  } else if (isStopLoss(symbol, lo)) {
-    acc.stopLosses.push(symbol);
-  }
-  return acc;
-};
 
 const processQuotes = async (quotes) => {
   const highsAndLosses = quotes.reduce(generateHighsAndLosses, {
@@ -54,6 +48,12 @@ const sendAlert = async (symbols) => {
   if (!symbols.length) return;
   const symbolsString = symbols.join(", ");
   console.error(`stop losses for ${symbolsString}`);
+};
+
+const updateHighs = (newHighs) => {
+  fs.writeFile("./highs.json", JSON.stringify(newHighs), "utf8", (err) => {
+    if (err) return console.error(err);
+  });
 };
 
 getStockPriceQuotes();
